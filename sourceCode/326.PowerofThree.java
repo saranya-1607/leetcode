b30e8e29539c878316418b5fb62f76ed702c7ef3
Saranya A <asaranya131@gmail.com>
2025-08-16 13:04:37 +0530

    Update 326.PowerofThree.java

--- sourceCode/326.PowerofThree.java
+++ sourceCode/326.PowerofThree.java
@@ -12,3 +12,22 @@ class 326.PowerofThree
         
     }
 }
+
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  class powerof3{
+    public static boolean pow(int n){
+        if(n<=0){
+            return false;
+        }
+        while(n%3==0){
+            n /= 3;
+        }
+        return n==1;
+        
+    }
+    public static void main(String[] args){
+        int n =20;
+        System.out.println(pow(n));
+    }
+}
