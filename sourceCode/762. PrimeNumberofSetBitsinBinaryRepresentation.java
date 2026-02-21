b1031d49fc9461840003dd8fd9d6cf7a799e2b1f
Saranya A <asaranya131@gmail.com>
2026-02-21 19:21:48 +0530

    Create 762. PrimeNumberofSetBitsinBinaryRepresentation.java

---
+++ sourceCode/762. PrimeNumberofSetBitsinBinaryRepresentation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int cnt =0;
+        for(int i=left;i<=right;i++){
+            int bits = Integer.bitCount(i);
+            if(isPrime(bits)){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+        private boolean isPrime(int n){
+            if(n<2) return false;
+            for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+}
