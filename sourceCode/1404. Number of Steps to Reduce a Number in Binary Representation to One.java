d45f489fda9a9d093df0fb3f3fd1ec5e35f6c955
Saranya A <asaranya131@gmail.com>
2026-02-26 12:03:25 +0530

    Create 1404. Number of Steps to Reduce a Number in Binary Representation to One.java

---
+++ sourceCode/1404. Number of Steps to Reduce a Number in Binary Representation to One.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSteps(String s) {
+         int steps=0;
+        int carry=0;
+       
+        for(int i=s.length()-1;i>0;i--){
+            int curr =s.charAt(i)-'0';
+            if(carry+curr==1){
+                steps +=2;
+                carry=1;
+            }else{
+                steps +=1;
+            }
+        }
+           return steps + carry;
+    }
+}
