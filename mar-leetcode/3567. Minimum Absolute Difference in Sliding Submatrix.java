5e3fc78f86a80e359204b3115120c29dcd04fef6
Saranya A <asaranya131@gmail.com>
2026-03-20 22:16:55 +0530

    Create 3567. Minimum Absolute Difference in Sliding Submatrix.java

---
+++ mar-leetcode/3567. Minimum Absolute Difference in Sliding Submatrix.java
@@ -0,0 +1,44 @@
+//Approach - Iterate and check all k*k matrices
+//T.C : O(O((m−k)×(n−k)×k^2×logk)
+//S.C : O(k^2)
+class Solution {
+    public int[][] minAbsDiff(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] result = new int[m - k + 1][n - k + 1];
+
+        for (int i = 0; i <= m - k; i++) {
+            for (int j = 0; j <= n - k; j++) {
+
+                // TreeSet for sorted + distinct elements
+                TreeSet<Integer> vals = new TreeSet<>();
+
+                for (int r = i; r <= i + k - 1; r++) {
+                    for (int c = j; c <= j + k - 1; c++) {
+                        vals.add(grid[r][c]);
+                    }
+                }
+
+                // If all elements are same
+                if (vals.size() == 1) {
+                    continue;
+                }
+
+                int minAbsDiff = Integer.MAX_VALUE;
+
+                Integer prev = null;
+                for (int val : vals) {
+                    if (prev != null) {
+                        minAbsDiff = Math.min(minAbsDiff, val - prev);
+                    }
+                    prev = val;
+                }
+
+                result[i][j] = minAbsDiff;
+            }
+        }
+
+        return result;
+    }
+}
